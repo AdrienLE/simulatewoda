e5d1d9788cb8810ac1a27da90ea134605bc47df1
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2012-03-29 00:31:28 +0200

    Colors in log, but now there's an error message at the beginning for some reason...

--- lib/log.rb
+++ lib/log.rb
@@ -1,4 +1,15 @@
 require 'logger'
+require 'colorize'
+
+String.class_eval do
+  def bold
+    if self =~ /^\e\[\d+;(\d+);(\d+)m(.*)\e\[0m$/
+      "\e[1;#{$1};#{$2}m#{$3}\e[0m"
+    else
+      "\e[1;39;49m#{self}\e[0m"
+    end
+  end
+end
 
 # TODO: Set the various log types through the command line arguments
 # TODO: Handle messages not in blocks
@@ -12,10 +23,24 @@ class Log < Logger
   def initialize
     super STDOUT
     @types = {}
+
+    old_formatter = self.formatter
+    self.formatter = Proc.new { |severity, time, progname, msg|
+#      return old_formatter.call severity, time, progname, msg unless severity == Logger::INFO
+      msg_str = msg.class == Proc ? msg.call.to_s : msg.to_s
+      time_str = time.strftime "%x %X"
+      type_str = @current_type.to_s
+      if @logdev.dev.tty?
+        type_str = type_str.blue.bold
+        time_str = time_str.light_yellow
+      end
+      "[#{time_str}] #{type_str}: #{msg_str}\n"
+    }
   end
 
   def log_info type, &msg
     return unless @types[type.to_sym]
+    @current_type = type
     self.info &msg
   end
 
